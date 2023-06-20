6b7e2fbbdce160d584a779f543cb017e89380ca0
Devether15 <josemoya.inf@gmail.com>
2023-06-20 10:14:39 +0100

    adding React context

--- src/routes/home/navigation/navigation.component.jsx
+++ src/routes/home/navigation/navigation.component.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrownLogo} from "../../../assets/crown.svg";
+import { UserContext } from "../../../contexts/user.context";
+import { signInOutUser } from "../../../utils/firebase/firebase.utils";
 import './navigation.styles.scss'
 
 const Navigation = () => {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    console.log("currentUser", currentUser);
+
+    const signOutHandler = async () => {
+        await signInOutUser();
+        setCurrentUser(null);
+    }
     return (
         <>
             <div className="navigation">
@@ -13,9 +23,12 @@ const Navigation = () => {
                     <Link  className="nav-link" to='/shop'>
                         SHOP
                     </Link>
-                    <Link  className="nav-link" to='/auth'>
-                        SIGN IN 
-                    </Link>
+                    { currentUser ? (
+                        <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+                    ) : (
+                        <Link  className="nav-link" to='/auth'>SIGN IN</Link>
+                    )}
+                    
                 </div>
             </div>
             <Outlet />
